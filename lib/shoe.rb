e87c45271f0bdc3bef360c6c6e99f973ca20903e
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-24 21:26:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,18 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
 
   BRANDS = []
   
   def initialize(brand)
     @brand = brand.uniq 
-    BRANDS << brand.uniq
+  end
+  
+  def brand=(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
   end
 
   def cobble
